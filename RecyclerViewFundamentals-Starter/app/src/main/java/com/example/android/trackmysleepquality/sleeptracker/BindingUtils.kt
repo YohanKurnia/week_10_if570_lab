61abcf8cd54625d1d099669d397b6365e5827dd5
YohanKurnia <yohankw7@gmail.com>
2023-05-09 21:44:28 +0700

    Handle item clicks

--- RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -9,18 +9,23 @@ import com.example.android.trackmysleepquality.database.SleepNight
 class BindingUtils {}
 
 @BindingAdapter("sleepDurationFormatted")
-fun TextView.setSleepDurationFormatted(item: SleepNight){
-    text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+fun TextView.setSleepDurationFormatted(item: SleepNight?){
+    item?.let {
+        text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+    }
 }
 
 @BindingAdapter("sleepQualityString")
-fun TextView.setSleepQualityString(item: SleepNight){
-    text = convertNumericQualityToString(item.sleepQuality, context.resources)
+fun TextView.setSleepQualityString(item: SleepNight?){
+    item?.let{
+        text = convertNumericQualityToString(item.sleepQuality, context.resources)
+    }
 }
 
 @BindingAdapter("sleepImage")
-fun ImageView.setSleepImage(item: SleepNight) {
-    setImageResource(when (item.sleepQuality) {
+fun ImageView.setSleepImage(item: SleepNight?) {
+    item?.let {
+        setImageResource(when (item.sleepQuality) {
             0 -> R.drawable.ic_sleep_0
             1 -> R.drawable.ic_sleep_1
             2 -> R.drawable.ic_sleep_2
@@ -28,5 +33,7 @@ fun ImageView.setSleepImage(item: SleepNight) {
             4 -> R.drawable.ic_sleep_4
             5 -> R.drawable.ic_sleep_5
             else -> R.drawable.ic_sleep_active
-    })
+        })
+    }
+
 }
